fbb9cf0d63774d8d7bef5fb8aa6af8a908da29c4
kym1229 <ymymym1229@gmail.com>
2021-03-05 13:15:19 +0900

    Implement BlockChain

---
+++ practiceBlockchain/BlockChain.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace practiceBlockchain
+{
+    public class BlockChain
+    {
+        static readonly Dictionary<byte[], Block> blocks 
+            = new Dictionary<byte[], Block>(new ByteArrayComparer());
+        private static long difficulty = 0;
+        private static long minimumDifficulty = 1024;
+
+        //minimumMultiplier, minimumDifficulty는 난이도 조절(difficulty 값 조절)을 위해
+
+        private static long difficultyBoundDivisor = 128;
+
+        public static byte[] makeBlock(
+            byte[] previousHash, 
+            byte[] hashValue, 
+            DateTimeOffset timeStamp, 
+            int nonce
+        )
+        {
+            if (previousHash is null)   //genesis block
+            {
+                previousHash = new byte[] { };
+                blocks.Add(hashValue, new Block(previousHash, timeStamp, nonce));
+            }
+            else
+            {
+                blocks.Add(hashValue, new Block(previousHash, timeStamp, nonce));
+                updateDifficulty(blocks[previousHash].TimeStamp, timeStamp);
+            }
+
+            return hashValue;
+        }
+
+        public static int Difficulty
+        {
+            get;
+        }
+
+        public static void updateDifficulty (
+            DateTimeOffset previouTimeStamp, 
+            DateTimeOffset currentTimeStamp
+        )
+        {
+            //EIP-2
+            //Libplanet/Blockchain/Policies/BlockPolicy.cs 참조 
+            long prevDifficulty = difficulty;
+            TimeSpan timeInterval = currentTimeStamp - previouTimeStamp;
+            const long minimumMultiplier = -99;
+            double multiplier = 
+                1 - timeInterval.TotalMilliseconds / difficultyBoundDivisor;
+            multiplier = Math.Max(multiplier, minimumMultiplier);
+
+            var offset = prevDifficulty / minimumDifficulty;
+            long nextDifficulty = Convert.ToInt64(prevDifficulty + (offset * multiplier));
+            difficulty = Math.Max(nextDifficulty, minimumDifficulty);
+
+            Console.WriteLine($"difficulty changed: {prevDifficulty} -> {difficulty}");
+        }
+
+        public static Block getBlock(byte[] hashValue)
+        {
+            Block returnedBlock = null;
+
+            try
+            {
+                returnedBlock = blocks[hashValue];
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+            return returnedBlock;
+        }
+
+        public static IEnumerable<byte[]> iterateBlock()
+        {
+            foreach(byte[] hashofBlock in blocks.Keys)
+            {
+                yield return hashofBlock;
+            }
+        }
+    }
+}
---
+++ practiceBlockchain/ByteArrayComparer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//byte[]끼리 비교해야 하는 경우 아예 자료형을 만들어서 비교
+//libplanet/Libplanet.Stun/Stun/TurnClient.cs
+namespace practiceBlockchain
+{
+    class ByteArrayComparer : IEqualityComparer<byte[]>
+    {
+        public bool Equals(byte[] x, byte[] y)
+        {
+            if (x == null || y == null)
+            {
+                return x == y;
+            }
+
+            return x.SequenceEqual(y);
+        }
+
+        public int GetHashCode(byte[] obj)
+        {
+            if (obj == null)
+            {
+                throw new ArgumentNullException(nameof(obj));
+            }
+
+            return obj.Sum(b => b);
+        }
+    }
+}
