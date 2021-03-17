87ccbddf8aa48145bcd6ff78fe24f55c1c92a8c3
kym1229 <ymymym1229@gmail.com>
2021-03-17 17:48:02 +0900

    no message

--- PracticeBlockChain/Block.cs
+++ PracticeBlockChain/Block.cs
@@ -71,9 +71,6 @@ namespace PracticeBlockChain
 
         public BigInteger Hash()
         {
-            // 기존에는 HashCash에서 찾은 nonce값을 해시함수에 넣어 현재 해시값을 얻는 형식이었는데
-            // 똑같은 해시값을 얻으려면 결국 nonce 뿐만 아니라 HashCash와 동일한 input을 넣어야 함
-            // 그걸 갖고 있는 게 Serialize 함수가 아닌가 해서 넣었는데.. 맞나
             SHA256 hashAlgo = SHA256.Create();
             BigInteger hashDigest = 
                 new BigInteger(hashAlgo.ComputeHash(
