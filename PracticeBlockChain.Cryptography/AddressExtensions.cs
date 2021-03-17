40f39f7ac01bc3521ff3a55ea01505d9ccd75ac5
kym1229 <ymymym1229@gmail.com>
2021-03-17 17:29:56 +0900

    Update structure of file

---
+++ PracticeBlockChain.Cryptography/AddressExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PracticeBlockChain.Cryptography
+{
+    public static class AddressExtensions
+    {
+        public static Address ToAddress(this PublicKey publicKey)
+        {
+            return new Address(publicKey);
+        }
+
+        public static Address ToAddress(this PrivateKey privateKey)
+        {
+            return new Address(privateKey.PublicKey);
+        }
+    }
+}
