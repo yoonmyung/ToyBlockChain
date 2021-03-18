39a1b1740b4b78e3dc0cb67462adf9c8a7aa54ad
kym1229 <ymymym1229@gmail.com>
2021-03-18 17:04:26 +0900

    Update get properties, MakeBlock and GetBlock function

--- PracticeBlockChain/BlockChain.cs
+++ PracticeBlockChain/BlockChain.cs
@@ -9,44 +9,82 @@ namespace PracticeBlockChain
 {
     public class BlockChain
     {
-        private readonly Dictionary<BigInteger, Block> blocks 
-            = new Dictionary<BigInteger, Block>();
+        private readonly Dictionary<byte[], Block> blocks 
+            = new Dictionary<byte[], Block>();
+        private byte[] hashofTipBlock;
         private readonly Block genesisBlock;
         private readonly long difficulty = 0;
 
         public BlockChain()
         {
-            MakeGenesisBlock();
+            genesisBlock = MakeGenesisBlock();
         }
 
-        private void MakeGenesisBlock()
+        public byte[] HashofTipBlock
+        {
+            get
+            {
+                return this.hashofTipBlock;
+            }
+        }
+
+        public long Difficulty
+        {
+            get; set;
+        }
+
+        private Block MakeGenesisBlock()
         {
             //빈 보드 상태
             Block block =
                 new Block(
                     index: 0,
-                    previousHash: 0,
+                    previousHash: null,
                     timeStamp: DateTimeOffset.Now,
                     nonce: new NonceGenerator().GenerateNonce(),
                     signature: null,
-                    state: new Board()
+                    payload: null
                 );
-            blocks.Add(0, block);
+            this.hashofTipBlock = block.Hash();
+            blocks.Add(hashofTipBlock, block);
+            return block;
         }
 
-        public void MakeBlock( BigInteger hashValue, Block block)
+        public void MakeBlock(byte[] hashValue, Block block)
         {
             // After validate block, then add the block to the blockchain.
             blocks.Add(hashValue, block);
-            Difficulty = DifficultyUpdater.UpdateDifficulty(
-                Difficulty, blocks[block.PreviousHash].TimeStamp, block.TimeStamp
-            );
+            hashofTipBlock = hashValue;
+//            Board.Put(block.State, player); 
+            if (block.PreviousHash is null)
+            {
+                Difficulty = DifficultyUpdater.UpdateDifficulty(
+                    Difficulty, genesisBlock.TimeStamp, block.TimeStamp
+                );
+            }
+            else
+            {
+                Difficulty = DifficultyUpdater.UpdateDifficulty(
+                    Difficulty, blocks[block.PreviousHash].TimeStamp, block.TimeStamp
+                );
+            }
         }
 
-        public Block GetBlock(BigInteger hashValue)
+        public Block GetBlock(byte[] hashValue)
         {
-            Block returnedBlock = blocks[hashValue];
-            return returnedBlock;
+            try
+            {
+                Block returnedBlock = blocks[hashValue];
+                return returnedBlock;
+            }
+            catch (KeyNotFoundException exception)
+            {
+                return null;
+            }
+            catch (ArgumentNullException exception)
+            {
+                return genesisBlock;
+            }
         }
 
         public IEnumerable<Block> IterateBlock()
@@ -55,11 +93,6 @@ namespace PracticeBlockChain
             {
                 yield return block;
             }
-        }
-
-        public long Difficulty
-        {
-            get; set;
         }
     }
 }
