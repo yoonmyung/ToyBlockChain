38ff1c2eea9d48be671f26cc242d4edb712e3517
kym1229 <ymymym1229@gmail.com>
2021-03-19 17:34:36 +0900

    Update test file about making block and updating state

--- PracticeBlockChain.Test/TictactoeGameTest.cs
+++ PracticeBlockChain.Test/TictactoeGameTest.cs
@@ -1,114 +1,225 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 using Xunit;
-using System.Numerics;
 using PracticeBlockChain.TicTacToeGame;
 using PracticeBlockChain.Cryptography;
+using System.Diagnostics;
 
 namespace PracticeBlockChain.Test
 {
     public class TictactoeGameTest
     {
-        private static Action MakeActionTest(
-            Player player,
-            Position position,
-            long txNonce
-        )
-        {
-            // player랑 position을 담은 정보를 payload에 넣은 다음에
-            // Action을 어차피 hashing하기 위해서 serialize를 하니까 
-            // payload는 굳이 byte배열로 저장할 필요가 없더라
-            (Player player, Position position) payload = (player, position);
-            Action action = new Action(
-                txNonce: txNonce,
-                signer: player.Address,
-                payload: payload
-            );
-            return action;
-        }
-
-        private static Nonce FindNonceTest(
-            Block previousBlock,
-            BigInteger previousHash,
-            BlockChain blockChain
-        )
-        {
-            Nonce nonce =
-                HashCash.CalculateHash(
-                    previousBlock: previousBlock,
-                    previousHash: previousHash,
-                    blockChain: blockChain
-                );
-            return nonce;
-        }
-
-        private static void AddBlocktoChainTest(
-            BlockChain blockChain,
-            Block block
-        )
-        {
-            blockChain.MakeBlock(hashValue: block.Hash(), block: block);
-        }
-
-        private static Position DeserializeActionTest(byte[] serializedPayload)
-        {
-            Position deserializedPayload =
-                (Position)Serialization.Deserialize(serializedPayload);
-            return deserializedPayload;
-        }
-
-        [Fact]
         public static void Main()
         {
-            var _blockIndex = 0;
-            var _player1TxNonce = 0;
-            var _player2TxNonce = 0;
-            var _board = new Board();
-            BlockChain _blockChain = new BlockChain();
-            Nonce _nonce = null;
+            long txNonce = 0;
+            long blockIndex = 1;
+            BlockChain blockChain = new BlockChain();
 
-            // 0. Player 생성
-            Player _player1 = new Player("Kim");
-            Player _player2 = new Player("Yoony");
+            // Initialize board.
+            string[,] board = InitializeBoard();
 
-            // Make a genesis block.
-            // Function here.
+            // Set the first player.
+            PrivateKey firstPlayerPrivateKey = new PrivateKey();
+            PublicKey firstPlayerPublicKey = firstPlayerPrivateKey.PublicKey;
+            Address firstPlayerAddress = new Address(firstPlayerPublicKey);
+            Assert.NotNull(firstPlayerAddress.AddressValue);
+            AddressPlayerMappingAttribute.AddPlayer(
+                address: firstPlayerAddress, 
+                playerName: "Kim"
+            );
 
-            // 1. Player1이 트랜젝션(여기선 액션)을 만들고 자신의 개인키로 서명한다.
-            Action _action = 
-                MakeActionTest(
-                    _player1, 
-                    new Position(1, 1),
-                    _player1TxNonce++
-                );
-            byte[] _signature = _player1.Sign(_action.Hash());
+            // Set the second player.
+            PrivateKey secondPlayerPrivateKey = new PrivateKey();
+            PublicKey secondPlayerPublicKey = secondPlayerPrivateKey.PublicKey;
+            Address secondPlayerAddress = new Address(secondPlayerPublicKey);
+            Assert.NotNull(secondPlayerAddress.AddressValue);
+            AddressPlayerMappingAttribute.AddPlayer(
+                address: secondPlayerAddress,
+                playerName: "Lee"
+            );
 
-            // 2. Player2는 Player1의 공개키로 이 트랜젝션을 검증한다.
-            bool isValidAction = 
-                _player2.Verify(_action.Hash(), _signature, _player1.PublicKey);
+            // Print Genesis block.
+            PrintBoard(board);
+            PrintTipofBlock(blockChain);
 
-            // 3. Player2의 검증을 통과하면, Player1은 작업증명을 수행한다.
-            if (isValidAction)
+            bool isFirstplayerTurn = true;
+            while (!StateController.IsEnd(board))
             {
-                _nonce = FindNonceTest(null, 0, null);
+                // Player
+                Position position = DecidePositiontoPut(
+                    (isFirstplayerTurn ? firstPlayerAddress : secondPlayerAddress), 
+                    txNonce
+                );
+                if(position.X < 0 || position.X > 2 || position.Y < 0 || position.Y > 2
+                    || !StateController.IsAbletoPut(board, position))
+                {
+                    Console.WriteLine(
+                        $"({position.X}, {position.Y})에 둘 수 없습니다"
+                    );
+                    continue;
+                }
+                // l
+                // l   Player는 BlockChain에게 자신이 수행한 것을 전달
+                // l
+                // ▼
+                // BlockChain
+                // Sign.
+                byte[] signature = 
+                    (isFirstplayerTurn? firstPlayerPrivateKey : secondPlayerPrivateKey)
+                    .Sign(
+                        new Action(
+                            txNonce: txNonce, 
+                            signer:
+                            (isFirstplayerTurn? firstPlayerAddress : secondPlayerAddress),
+                            payload: position, 
+                            signature: null
+                        ).Hash()
+                    );
+                // Verify. 
+                // Suppose that the player who isn't this turn 
+                // tries to verify the action of this turn player.
+                Action action = 
+                    new Action(
+                        txNonce: txNonce, 
+                        signer: 
+                        (isFirstplayerTurn? firstPlayerAddress : secondPlayerAddress), 
+                        payload: position, 
+                        signature: signature
+                    );
+                bool isValidAction =
+                    (isFirstplayerTurn? firstPlayerPublicKey : secondPlayerPublicKey)
+                    .Verify(action.Hash(), action.Signature);
+                Assert.True(isValidAction);
+
+                // Proof of work.
+                Nonce nonce =
+                    HashCash.CalculateHash(
+                        previousBlock: blockChain.GetBlock(blockChain.HashofTipBlock),
+                        blockChain: blockChain
+                    );
+
+                // Need Validation.
+                // If pass the validation, make block (with executing action).
+                Block block = new Block(
+                    index: blockChain.GetBlock(blockChain.HashofTipBlock).Index + 1,
+                    previousHash: blockChain.HashofTipBlock,
+                    timeStamp: DateTimeOffset.Now,
+                    nonce: nonce,
+                    action: action
+                );
+                // first "board" = next state, second "board" = current state.
+                board = blockChain.AddBlock(block, board);
+                isFirstplayerTurn = !isFirstplayerTurn;
+
+                //Print current block.
+                PrintBoard(board);
+                PrintTipofBlock(blockChain);
             }
-            Block _block = new Block(
-                index: _blockIndex++,
-                previousHash: 0,
-                timeStamp: DateTimeOffset.Now,
-                nonce: _nonce,
-                signature: _signature,
-                state: _board
-            );
+        }
 
-            // 4. 만족하는 논스를 찾은 논스와 당시 difficulty, 해시값이 유효한지 Validation을 거친다.
-            // Validation
+        private static string[,] InitializeBoard()
+        {
+            string[,] board = new string[3, 3];
+            for (var row = 0; row < 3; row++)
+            {
+                for (var calmn = 0; calmn < 3; calmn++)
+                {
+                    board[row, calmn] = "";
+                }
+            }
+            return board;
+        }
 
-            // 5. Validation을 통과하면 블록체인에 블록을 생성한다 + 동시에 트랜젝션을 execute한다.
-            AddBlocktoChainTest(
-                _blockChain, _block
+        private static Position DecidePositiontoPut(Address address, long txNonce)
+        {
+            Console.Write(
+                $"{AddressPlayerMappingAttribute.GetPlayer(address)}이 이동할 위치 입력: "
             );
+            // Input "5 3" means player will put his tuple on the (5, 3).
+            string[] input = Console.ReadLine().Split(' ');
+            return new Position(int.Parse(input[0]), int.Parse(input[1]));
+        }
+
+        private static void PrintBoard(string[,] board)
+        {
+            Console.WriteLine();
+            Console.WriteLine("---------------------------");
+            for(var row = 0; row < 3; row++)
+            {
+                for(var column = 0; column < 3; column++)
+                {
+                    if (board[row, column].Length > 0)
+                    {
+                        Console.Write($"   {board[row, column]}   ");
+                    }
+                    else
+                    {
+                        Console.Write($"         ");
+                    }
+                }
+                Console.WriteLine("\n---------------------------\n");
+            }
+        }
+
+        private static void PrintTipofBlock(BlockChain blockChain)
+        {
+            Debug.WriteLine("-----------------------------------------------");
+            Debug.Write("Block #");
+            Debug.WriteLine(blockChain.GetBlock(blockChain.HashofTipBlock).Index);
+            Debug.Write("Nonce: ");
+            try
+            {
+                Debug.WriteLine(
+                    String.Join(
+                        " ",
+                        blockChain.GetBlock(blockChain.HashofTipBlock).Nonce.NonceValue
+                    )
+                );
+            } catch(Exception exception)
+            {
+                Debug.WriteLine("null");
+            }
+            Debug.Write("Previous Hash: ");
+            if (!(blockChain.GetBlock(blockChain.HashofTipBlock).PreviousHash is null))
+            {
+                Debug.WriteLine(
+                    String.Join(
+                        " ", 
+                        blockChain.GetBlock(blockChain.HashofTipBlock).PreviousHash
+                    )
+                );
+            }
+            else
+            {
+                Debug.WriteLine("null");
+            }
+            Debug.Write("Current Hash: ");
+            Debug.WriteLine(
+                String.Join(
+                    " ", 
+                    blockChain.GetBlock(blockChain.HashofTipBlock).Hash()
+                )
+            );
+            Debug.Write("TimeStamp: ");
+            Debug.WriteLine(blockChain.GetBlock(blockChain.HashofTipBlock).TimeStamp);
+            Debug.Write("Action: ");
+            if (!(blockChain.GetBlock(blockChain.HashofTipBlock).GetAction is null))
+            {
+                Debug.WriteLine(
+                    "Move to (" +
+                    blockChain.GetBlock(blockChain.HashofTipBlock).GetAction.Payload.X +
+                    ", " +
+                    blockChain.GetBlock(blockChain.HashofTipBlock).GetAction.Payload.Y +
+                    ")"
+                );
+            }
+            else
+            {
+                Debug.WriteLine("null");
+            }
+            Debug.Write("Difficulty: ");
+            Debug.WriteLine(blockChain.Difficulty);
+            Debug.WriteLine("-----------------------------------------------");
         }
     }
 }
