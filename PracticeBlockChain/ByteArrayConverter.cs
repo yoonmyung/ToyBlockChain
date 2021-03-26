05d59fbbbd63b08d4eb3b7334070d5ecbbdc7858
kym1229 <ymymym1229@gmail.com>
2021-03-26 17:44:19 +0900

    Add file

---
+++ PracticeBlockChain/ByteArrayConverter.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace PracticeBlockChain
+{
+    public static class ByteArrayConverter
+    {
+        public static Object DeSerialize(byte[] arrBytes)
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                var binaryFormatter = new BinaryFormatter();
+                var decompressed = Decompress(arrBytes);
+
+                memoryStream.Write(decompressed, 0, decompressed.Length);
+                memoryStream.Seek(0, SeekOrigin.Begin);
+
+                return binaryFormatter.Deserialize(memoryStream);
+            }
+        }
+
+        public static byte[] Compress(byte[] input)
+        {
+            byte[] compressesData;
+
+            using (var outputStream = new MemoryStream())
+            {
+                using (var zip = new GZipStream(outputStream, CompressionMode.Compress))
+                {
+                    zip.Write(input, 0, input.Length);
+                }
+
+                compressesData = outputStream.ToArray();
+            }
+
+            return compressesData;
+        }
+
+        public static byte[] Decompress(byte[] input)
+        {
+            byte[] decompressedData;
+
+            using (var outputStream = new MemoryStream())
+            {
+                using (var inputStream = new MemoryStream(input))
+                {
+                    using 
+                    (
+                        var zip = 
+                            new GZipStream(inputStream, CompressionMode.Decompress)
+                    )
+                    {
+                        zip.CopyTo(outputStream);
+                    }
+                }
+
+                decompressedData = outputStream.ToArray();
+            }
+
+            return decompressedData;
+        }
+    }
+}
