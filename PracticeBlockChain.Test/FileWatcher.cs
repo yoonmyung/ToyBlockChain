0545044b445f2165b3ad9d965c95685737b4ee49
DESKTOP-PM37DLV\Planetarium <ymymym1229@gmail.com>
2021-03-31 18:08:34 +0900

    Add playerAddress param to control multi processes

--- PracticeBlockChain.Test/FileWatcher.cs
+++ PracticeBlockChain.Test/FileWatcher.cs
@@ -1,11 +1,15 @@
-﻿using System;
+﻿using PracticeBlockChain.Cryptography;
+using PracticeBlockChain.TicTacToeGame;
+using System;
 using System.IO;
+using System.Linq;
+using System.Threading;
 
 namespace PracticeBlockChain.Test
 {
     public static class FileWatcher
     {
-        public static void RunWatcher(BlockChain blockChain)
+        public static void RunWatcher(BlockChain blockChain, Address address)
         {
             var directoryWatcher = new FileSystemWatcher();
 
@@ -21,13 +25,45 @@ namespace PracticeBlockChain.Test
                 | NotifyFilters.LastWrite
                 | NotifyFilters.Security
                 | NotifyFilters.Size;
-            var handler = new FileSystemEventHandler((s, e) => WatcherOnChanged(s, e, blockChain));
+            var handler = 
+                new FileSystemEventHandler((s, e) => WatcherOnChanged(s, e, blockChain, address));
             directoryWatcher.Created += handler;
             directoryWatcher.EnableRaisingEvents = true;
         }
 
-        private static void WatcherOnChanged(object sender, FileSystemEventArgs e, BlockChain blockChain)
+        private static void WatcherOnChanged
+        (object sender, FileSystemEventArgs e, BlockChain blockChain, Address address)
         {
+            if 
+            (
+                blockChain.TipBlock.GetAction.Signer.AddressValue.SequenceEqual
+                (
+                    blockChain.GenesisBlock.GetAction.Signer.AddressValue
+                )
+            )
+            {
+                // Genesis block 상태에서는 항상 Lee가 먼저 두는 상태
+                if (AddressPlayerMappingAttribute.GetPlayer(address) == "Kim")
+                {
+                    Thread.Sleep(1000);
+                }
+                else
+                {
+                    Thread.Sleep(100);
+                }
+            }
+            else if
+            (
+                address.AddressValue.SequenceEqual(blockChain.TipBlock.GetAction.Signer.AddressValue)
+            )
+            {
+                // Tip이 현재 생성된 블록으로 갱신되기 전의 블록(이전 턴에 말을 둔 플레이어)
+                Thread.Sleep(1000);
+            }
+            else
+            {
+                Thread.Sleep(100);
+            }
             Console.WriteLine();
             TictactoeGameTest.PrintCurrentState(blockChain);
         }
