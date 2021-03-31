07c373568a5a9f65fc252c8f69ad357069202076
DESKTOP-PM37DLV\Planetarium <ymymym1229@gmail.com>
2021-03-31 15:45:29 +0900

    Add FileWatcher for synchronize states

---
+++ PracticeBlockChain.Test/FileWatcher.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace PracticeBlockChain.Test
+{
+    public static class FileWatcher
+    {
+        public static void RunWatcher(BlockChain blockChain)
+        {
+            var directoryWatcher = new FileSystemWatcher();
+
+            directoryWatcher.Filter = "*.*";
+            directoryWatcher.Path = blockChain.StateStorage;
+            directoryWatcher.IncludeSubdirectories = true;
+            directoryWatcher.NotifyFilter = 
+                NotifyFilters.Attributes
+                | NotifyFilters.CreationTime
+                | NotifyFilters.DirectoryName
+                | NotifyFilters.FileName
+                | NotifyFilters.LastAccess
+                | NotifyFilters.LastWrite
+                | NotifyFilters.Security
+                | NotifyFilters.Size;
+            var handler = new FileSystemEventHandler((s, e) => WatcherOnChanged(s, e, blockChain));
+            directoryWatcher.Created += handler;
+            directoryWatcher.EnableRaisingEvents = true;
+        }
+
+        private static void WatcherOnChanged(object sender, FileSystemEventArgs e, BlockChain blockChain)
+        {
+            Console.WriteLine();
+            TictactoeGameTest.PrintCurrentState(blockChain);
+        }
+    }
+}
