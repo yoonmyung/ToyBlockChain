3f5a9b5bffadf3c3079e1d5f0b390a2d6a39aee9
DESKTOP-PM37DLV\Planetarium <ymymym1229@gmail.com>
2021-04-07 21:17:55 +0900

    Add test file to check TCP/IP network between seed node and node

---
+++ PracticeBlockChain.Test/NetworkTest.cs
@@ -0,0 +1,30 @@
+﻿using PracticeBlockChain.Network;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace PracticeBlockChain.Test
+{
+    public class NetworkTest
+    {
+        public static void Main(string[] args)
+        {
+            if (args[0].Equals("8888"))
+            {
+                var seedNodeThread = new Thread(StartSeedNode);
+                seedNodeThread.Start();
+            }
+            else
+            {
+                var node = new Node(int.Parse(args[0]));
+                node.ConnectToSeedNode();
+            }
+        }
+
+        private static void StartSeedNode()
+        {
+            var seedNode = new Node();
+        }
+    }
+}
