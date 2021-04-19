2bf32c372cac28af4422851f6789f9dd6782c102
kym1229 <ymymym1229@gmail.com>
2021-04-19 17:26:31 +0900

    Add method to make action to transport

--- PracticeBlockChain.Test/NetworkTest.cs
+++ PracticeBlockChain.Test/NetworkTest.cs
@@ -13,5 +13,47 @@ namespace PracticeBlockChain.Test
                 node.RotateRoutingTable();
             }
         }
+
+        private static void SendAction
+        (
+            object lockThis, PrivateKey privateKey, BlockChain blockChain, Node node
+        )
+        {
+            lock (lockThis)
+            {
+                var action = MakeAction(privateKey, blockChain);
+                node.RotateRoutingTable
+                (
+                    new ArrayList { privateKey.PublicKey.Format(true), action.Signature, action.ActionId }
+                );
+            }
+        }
+
+        private static Action MakeAction(PrivateKey privateKey, BlockChain blockChain)
+        {
+            var address = new Address(privateKey.PublicKey);
+            byte[] signature =
+                privateKey.Sign
+                (
+                    new Action
+                    (
+                        txNonce: blockChain.GetHowmanyBlocksMinermade(address) + 1,
+                        signer: address,
+                        payload: null,
+                        signature: null
+                    ).Hash()
+                );
+            var action =
+                new Action
+                (
+                    txNonce:
+                    blockChain.GetHowmanyBlocksMinermade(address) + 1,
+                    signer: address,
+                    payload: null,
+                    signature: signature
+                );
+
+            return action;
+        }
     }
 }
